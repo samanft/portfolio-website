f430cf8b04d0c367b57c3bc016f633b29bc9632f
samanft <poopeekaska@gmail.com>
2024-02-05 00:17:11 +0100

    added new section + small ui update

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    theme: {
+      extend: {
+        screens: {
+          '3xl': '1600px',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
